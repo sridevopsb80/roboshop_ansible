983da73c46b895697699af377df67f562c8a0ae0
srikvp <93551165+srikvp@users.noreply.github.com>
2024-11-05 11:27:35 +0530

    env variable defined

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#this shell script can be used to execute the rolename defined in main.yml
+#this shell script can be used to execute the rolename defined in main.yml. env variable is being passed during the execution of the command
 
 git pull
 ansible-playbook -i  $1.dev.sridevops.site, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
