38b01567eb9e486ffaae9f5e64b6637cc2b48c0e
srikvp <93551165+srikvp@users.noreply.github.com>
2024-11-19 21:40:11 +0530

    updated comments

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 #Executing make frontend will execute the frontend defined here. for other components, define as necessary
-#
+#tab space is used for git pull
+
+#command used when main.yml is executed in cli:
+#git pull; ansible-playbook -i frontend.dev.sridevops.site, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=frontend main.yml
 
 frontend:
 	git pull
--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
+
+
 #this shell script can be used to execute the rolename defined in main.yml. env variable is being passed during the execution of the command
 
 git pull
 ansible-playbook -i  $1.dev.sridevops.site, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
+
+#command used when main.yml is executed in cli:
+#git pull; ansible-playbook -i frontend.dev.sridevops.site, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=frontend main.yml
+
+# $1 value has to be passed while executing the run.sh.
+# command to execute in cli: bash run.sh frontend
