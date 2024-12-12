2c0b0500d72a58af42074c955f35077b8cb7a44c
srikvp <93551165+srikvp@users.noreply.github.com>
2024-12-12 17:59:11 +0530

    update shell

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
+git pull
+ansible-playbook -i  $1.dev.sridevops.site, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml -e vault_token=$2
 
 
 #this shell script can be used to execute the rolename defined in main.yml. env variable is being passed during the execution of the command
-
-git pull
-ansible-playbook -i  $1.dev.sridevops.site, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml -e vault_token=$2
 
 #command used when main.yml is executed in cli:
 #git pull; ansible-playbook -i frontend.dev.sridevops.site, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=frontend main.yml <root token value of vault>
